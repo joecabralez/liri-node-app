d6a7f4109cec6ff9c24557eebb1db8b1752ae614
Joe <cabralez07@gmail.com>
2017-05-12 08:25:46 -0500

    5.12 morning

--- liri.js
+++ liri.js
@@ -82,3 +82,20 @@ spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(er
  
     // Do something with 'data' 
 });
+
+////////////////////
+//~READ TEXT FILE~//
+////////////////////
+
+// fs is an NPM package for reading and writing files
+var fs = require("fs");
+
+// This block of code will read from the "movies.txt" file.
+// It's important to include the "utf8" parameter or the code will provide stream data (garbage)
+// The code will store the contents of the reading inside the variable "data"
+fs.readFile("random.txt", "utf8", function(error, data) {
+
+  // We will then print the contents of data
+  console.log(data);
+
+});
